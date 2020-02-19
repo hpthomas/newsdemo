0466393dc0c4668d45b4cfd7809e0c5d4cb6021c
Hugh Thomas <hugh902@gmail.com>
2020-02-19 15:49:55 -0600

    demo initial

---
+++ precache-manifest.5c6f44b4065dd17ac478041e209f0258.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1d23c7611195ce27f7bccfdee721ac1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d7e72282c5a8b138b0a",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "98f594bd4e015cbef519",
+    "url": "/static/css/main.b57eaede.chunk.css"
+  },
+  {
+    "revision": "9d7e72282c5a8b138b0a",
+    "url": "/static/js/2.4d7c0550.chunk.js"
+  },
+  {
+    "revision": "98f594bd4e015cbef519",
+    "url": "/static/js/main.fa001571.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
